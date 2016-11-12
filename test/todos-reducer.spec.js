a4ddaf7414f73e1326267340bc75213a7b1d0d2b
Marton Hipszki <mhipszki@gmail.com>
2016-11-12 13:35:50 +0000

    todos reducer can add a todo to the list

---
+++ test/todos-reducer.spec.js
@@ -0,0 +1,30 @@
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: false
+                }
+            ]
+        default:
+            return state;
+        }
+};
+
+test('can add a todo to the list', () => {
+    const stateBefore = [];
+    const action = {
+        type: 'ADD_TODO',
+        id: 0,
+        text: 'first thing to do'
+    };
+    const stateAfter = [{
+        id: 0,
+        text: 'first thing to do',
+        completed: false
+    }];
+    expect(todos(stateBefore, action)).toEqual(stateAfter);
+});
