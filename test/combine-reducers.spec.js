b09caeb4147f45621522b52a43c0fa89cb72077f
Marton Hipszki <mhipszki@gmail.com>
2016-11-12 21:13:39 +0000

    combined reducer sets initial state

--- test/combine-reducers.spec.js
+++ test/combine-reducers.spec.js
@@ -1,5 +1,31 @@
-const combineReducers = () => () => {};
+const combineReducers = (reducers) => {
+    return (state = {}, action) => {
+        return Object.keys(reducers).reduce(
+            (nextState, key) => {
+                nextState[key] = reducers[key](
+                    state[key],
+                    action
+                );
+                return nextState;
+            },
+            {}
+        );
+    };
+};
 
 test('returns a function', () => {
     expect(typeof combineReducers()).toBe('function');
+});
+
+test('combined reducer sets initial state', () => {
+    const stateBefore = undefined;
+    const action = {};
+    const stateAfter = {
+        one: 1,
+        two: 2
+    };
+    const one = (state = 1, action) => state;
+    const two = (state = 2, action) => state;
+    const reducer = combineReducers({ one, two });
+    expect(reducer(stateBefore, action)).toEqual(stateAfter);
 });
