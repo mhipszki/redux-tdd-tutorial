bec8d26d14217a94c48a67c7a29360634ee6221b
Marton Hipszki <mhipszki@gmail.com>
2016-11-12 12:48:07 +0000

    app uses React to render the counter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import counter from './counter-reducer';
 
 const store = createStore(counter);
 
+const Counter = ({
+    value,
+    onIncrement,
+    onDecrement
+}) => (
+    <div>
+        <h1>{value}</h1>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+    </div>
+);
+
 const render = () => {
-    document.body.innerText = store.getState();
+    ReactDOM.render(
+        <Counter
+            value={store.getState()}
+            onIncrement={() => {
+                store.dispatch({
+                    type: 'INCREMENT'
+                })
+            }}
+            onDecrement={() => {
+                store.dispatch({
+                    type: 'DECREMENT'
+                })
+            }}
+        />,
+        document.getElementById('root')
+    );
 };
 
 store.subscribe(render);
 
 render();
-
-document.addEventListener('click', () => {
-    store.dispatch({ type: 'INCREMENT' });
-});
