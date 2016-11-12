b16e86ef5a32a3df2f2b8f806868395da0e3bdf6
Marton Hipszki <mhipszki@gmail.com>
2016-11-12 11:54:35 +0000

    store subscribe() returns a function to unsubscribe listener

--- src/store/create.js
+++ src/store/create.js
@@ -1,6 +1,6 @@
 const createStore = (reducer) => {
     let state;
-    const listeners = [];
+    let listeners = [];
     const getState = () => state;
     const dispatch = (action) => {
         state = reducer(state, action);
@@ -8,6 +8,9 @@ const createStore = (reducer) => {
     };
     const subscribe = (listener) => {
         listeners.push(listener);
+        return () => {
+            listeners = listeners.filter(l => l !== listener);
+        }
     };
     dispatch({});
     return { getState, dispatch, subscribe };
--- test/store/subscribe.spec.js
+++ test/store/subscribe.spec.js
@@ -8,3 +8,19 @@ test('subscribes a listener to state changes', () => {
     store.dispatch();
     expect(listener).toBeCalled();
 });
+
+test('returns a function to unsubscribe listener', () => {
+    const listeners = [ jest.fn(()=>1), jest.fn(()=>2) ];
+    const reducer = () => {};
+    const store = createStore(reducer);
+    const unsubscribe = store.subscribe(listeners[0]);
+    store.subscribe(listeners[1]);
+    store.dispatch();
+    expect(listeners[0]).toBeCalled();
+    expect(listeners[1]).toBeCalled();
+    jest.clearAllMocks();
+    unsubscribe();
+    store.dispatch();
+    expect(listeners[0]).not.toBeCalled();
+    expect(listeners[1]).toBeCalled();
+});
