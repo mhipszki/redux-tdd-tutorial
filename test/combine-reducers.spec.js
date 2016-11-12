64e1ef2d3648135bad0c225c6faad4b359aacc22
Marton Hipszki <mhipszki@gmail.com>
2016-11-12 21:21:02 +0000

    combineReducers can combine multiple levels of reducers

--- test/combine-reducers.spec.js
+++ test/combine-reducers.spec.js
@@ -54,3 +54,21 @@ test('combined reducer updates given part of state', () => {
     const reducer = combineReducers({ one, two });
     expect(reducer(stateBefore, action)).toEqual(stateAfter);
 });
+
+test('can combine multiple levels of reducers', () => {
+    const stateBefore = {};
+    const action = {};
+    const stateAfter = {
+        one: 1,
+        two: {
+            three: 3,
+            four: 4
+        }
+    };
+    const one = (state = 1, action) => state;
+    const three = (state = 3, action) => state;
+    const four = (state = 4, action) => state;
+    const two = combineReducers({ three, four });
+    const reducer = combineReducers({ one, two });
+    expect(reducer(stateBefore, action)).toEqual(stateAfter);
+});
