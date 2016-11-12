81b8c34797ff7f0bb32bf1beb3112d0832130752
Marton Hipszki <mhipszki@gmail.com>
2016-11-12 18:50:48 +0000

    moved todos reducer to its own module

--- test/todos-reducer.spec.js
+++ test/todos-reducer.spec.js
@@ -1,30 +1,5 @@
+import todos from '../src/todos-reducer';
 import deepFreeze from 'deep-freeze';
-
-const todos = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return [
-                ...state,
-                {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                }
-            ];
-        case 'TOGGLE_TODO':
-            return state.map(todo => {
-                if (todo.id !== action.id) {
-                    return todo;
-                }
-                return {
-                    ...todo,
-                    completed: !todo.completed
-                }
-            });
-        default:
-            return state;
-        }
-};
 
 test('can add a todo to the list', () => {
     const stateBefore = [];
