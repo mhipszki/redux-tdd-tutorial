2416665b6b77fdb579beabc1f65a6a8dce578bb4
Marton Hipszki <mhipszki@gmail.com>
2016-11-12 19:06:00 +0000

    todo reducer can toggle a todo

--- test/todo-reducer.spec.js
+++ test/todo-reducer.spec.js
@@ -8,6 +8,11 @@ const todo = (state, action) => {
                 text: action.text,
                 completed: false
             };
+        case 'TOGGLE_TODO':
+            return {
+                ...state,
+                completed: !state.completed
+            }
         default:
             return state;
     }
@@ -24,6 +29,24 @@ test('can create a new todo', () => {
         id: 0,
         text: 'new todo',
         completed: false
+    };
+    deepFreeze(action);
+    expect(todo(stateBefore, action)).toEqual(stateAfter);
+});
+
+test('can toggle a todo', () => {
+    const stateBefore = {
+        id: 0,
+        text: 'a todo',
+        completed: false
+    };
+    const action = {
+        type: 'TOGGLE_TODO'
+    };
+    const stateAfter = {
+        id: 0,
+        text: 'a todo',
+        completed: true
     };
     deepFreeze(action);
     expect(todo(stateBefore, action)).toEqual(stateAfter);
