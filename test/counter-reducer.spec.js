7742d1d44c6e1e95fd24692ac70bfe74654faa06
Marton Hipszki <mhipszki@gmail.com>
2016-11-12 10:10:25 +0000

    counter reducer returns state when receives unknown action type

--- test/counter-reducer.spec.js
+++ test/counter-reducer.spec.js
@@ -3,6 +3,8 @@ const counter = (state, action) => {
         return state + 1;
     } else if (action.type === 'DECREMENT') {
         return state - 1;
+    } else {
+        return state;
     }
 };
 
@@ -14,4 +16,8 @@ test('increments state', () => {
 test('decrements state', () => {
     expect(counter(2, { type: 'DECREMENT' })).toBe(1);
     expect(counter(1, { type: 'DECREMENT' })).toBe(0);
+});
+
+test('returns current state when receives unknown action type', () => {
+    expect(counter(0, { type: 'UNKNOWN' })).toBe(0);
 });
