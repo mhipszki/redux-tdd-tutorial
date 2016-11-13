ee6a3b6bb51750744bb231cbbb3708a17b36b645
Marton Hipszki <mhipszki@gmail.com>
2016-11-13 21:41:32 +0000

    getVisibleTodos returns active todos when receives filter SHOW_ACTIVE

--- test/todo/get-visible-todos.spec.js
+++ test/todo/get-visible-todos.spec.js
@@ -4,6 +4,8 @@ const getVisibleTodos = (todos, filter) => {
             return todos;
         case 'SHOW_COMPLETED':
             return todos.filter(t => t.completed);
+        case 'SHOW_ACTIVE':
+            return todos.filter(t => !t.completed);
     }
 };
 
@@ -26,5 +28,17 @@ test.only('returns completed todos when receives SHOW_COMPLETED filter', () => {
     const todos = getVisibleTodos(allTodos, 'SHOW_COMPLETED');
     expect(todos).toEqual([{
         completed: true
+    }]);
+});
+
+test.only('returns active todos when receives SHOW_ACTIVE filter', () => {
+    const allTodos = [{
+        completed: false
+    },{
+        completed: true
+    }];
+    const todos = getVisibleTodos(allTodos, 'SHOW_ACTIVE');
+    expect(todos).toEqual([{
+        completed: false
     }]);
 });
