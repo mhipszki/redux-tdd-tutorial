63306167524ec83488bf943969bcefac7829a76f
Marton Hipszki <mhipszki@gmail.com>
2016-11-13 21:12:41 +0000

    FilterLink component sets visibility filter when clicked

--- test/todo/components/filter-link.spec.js
+++ test/todo/components/filter-link.spec.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { createStore } from 'redux';
+import visibilityFilter from '../../../src/reducer/visibility-filter';
 
-const FilterLink = ({ children }) => (
-    <a href="#">{children}</a>
+const FilterLink = ({ store, filter, children }) => (
+    <a href="#" onClick={() => {
+        store.dispatch({
+            type: 'SET_VISIBILITY_FILTER',
+            filter: filter
+        })
+    }}>{children}</a>
+);
+
+const store = createStore(visibilityFilter);
+
+const render = () => shallow(
+    <FilterLink store={store} filter={'filter'}>
+        filter name
+    </FilterLink>
 );
 
 test('renders an anchor', () => {
@@ -11,11 +26,17 @@ test('renders an anchor', () => {
 });
 
 test('renders its children', () => {
-    const link = shallow(<FilterLink>children</FilterLink>)
-    expect(link.render().text()).toEqual('children');
+    const link = render();
+    expect(link.render().text()).toEqual('filter name');
 });
 
 test('points to the same page', () => {
-    const link = shallow(<FilterLink>children</FilterLink>)
+    const link = render();
     expect(link.prop('href')).toEqual('#');
+});
+
+test('sets visibility filter when clicked', () => {
+    const link = render();
+    link.simulate('click');
+    expect(store.getState()).toEqual('filter');
 });
