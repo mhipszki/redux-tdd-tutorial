04c66a2b3c8a6fe3180dbf082535b434158b508d
Marton Hipszki <mhipszki@gmail.com>
2016-11-13 20:59:05 +0000

    FilterLink component renders its children

--- test/todo/components/filter-link.spec.js
+++ test/todo/components/filter-link.spec.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-const FilterLink = () => (
-    <a/>
+const FilterLink = ({ children }) => (
+    <a>{children}</a>
 );
 
 test('renders an anchor', () => {
     const link = shallow(<FilterLink/>)
     expect(link.type()).toEqual('a');
+});
+
+test('renders its children', () => {
+    const link = shallow(<FilterLink>children</FilterLink>)
+    expect(link.render().text()).toEqual('children');
 });
