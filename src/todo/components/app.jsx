8850a2ee817598dcbe2b97707a0c85a56e17e2c1
Marton Hipszki <mhipszki@gmail.com>
2016-11-13 21:25:25 +0000

    added some formatting to TodoApp filter links

--- src/todo/components/app.jsx
+++ src/todo/components/app.jsx
@@ -37,8 +37,12 @@ const TodoApp = ({ store }) => {
                 {todos}
             </ul>
             <p>
+                Show
+                {' '}
                 <FilterLink store={store} filter="SHOW_ALL">All</FilterLink>
+                {' '}
                 <FilterLink store={store} filter="SHOW_ACTIVE">Active</FilterLink>
+                {' '}
                 <FilterLink store={store} filter="SHOW_COMPLETED">Completed</FilterLink>
             </p>
         </div>
