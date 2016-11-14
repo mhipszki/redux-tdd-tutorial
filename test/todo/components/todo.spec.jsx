49098a11154c5d87962a41912dfbaadd385d3a8d
Marton Hipszki <mhipszki@gmail.com>
2016-11-14 09:20:16 +0000

    Todo component renders a list item

--- test/todo/components/todo.spec.jsx
+++ test/todo/components/todo.spec.jsx
@@ -1,34 +1,11 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import { createStore } from 'redux';
-import TodoApp from '../../../src/todo/components/app'
-import reducer from '../../../src/reducer/app';
+import { shallow } from 'enzyme';
 
-let store;
-let app;
-let todo;
+const Todo = () => (
+    <li></li>
+);
 
-beforeEach(() => {
-    store = createStore(reducer);
-    app = mount(<TodoApp store={store} />);
-    store.dispatch({
-        type: 'ADD_TODO',
-        id: 0,
-        text: 'new todo'
-    });
-    app.update();
-    todo = app.find('li').first();
-});
-
-test('toggles completed on click', () => {
-    todo.simulate('click');
-    const todos = store.getState().todos;
-    expect(todos[0].completed).toBe(true);
-});
-
-test('is crossed when completed', () => {
-    expect(todo.prop('style').textDecoration).toBe('none');
-    todo.simulate('click');
-    app.update();
-    expect(todo.prop('style').textDecoration).toBe('line-through');
+test('renders a list element', () => {
+    const todo = shallow(<Todo/>);
+    expect(todo.type()).toBe('li');
 });
