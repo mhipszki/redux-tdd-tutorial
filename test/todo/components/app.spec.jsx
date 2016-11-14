47fbde422d2d0c6b614d7721b4cc7747c3e6b86a
Marton Hipszki <mhipszki@gmail.com>
2016-11-14 11:44:05 +0000

    TodoApp toggles todos on click

--- test/todo/components/app.spec.jsx
+++ test/todo/components/app.spec.jsx
@@ -57,6 +57,19 @@ test('renders the visible list of todos', () => {
     expect(todoList.prop('todos')).toBe(visibleTodos);
 });
 
+test('toggles todos on click', () => {
+    const store = createStore(reducer);
+    const app = mount(<TodoApp store={store} />);
+    const input = app.find('input');
+    const button = app.find('button');
+    input.node.value = 'New Todo';
+    button.simulate('click');
+    app.update();
+    const todo = app.find('Todo');
+    todo.simulate('click');
+    expect(store.getState().todos[0].completed).toBe(true);
+});
+
 test('shows a filter link to show all todos', () => {
     const store = createStore(reducer);
     const currentFilter = store.getState().visibilityFilter;
