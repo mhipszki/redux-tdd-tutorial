be53437c6a622c9e4541e18c28201da3075276be
Marton Hipszki <mhipszki@gmail.com>
2016-11-14 12:22:57 +0000

    AddTodo component renders a button to add a new todo

--- test/todo/components/add-todo.spec.jsx
+++ test/todo/components/add-todo.spec.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 const AddTodo = () => (
-    <input/>
+    <div>
+        <input/>
+        <button>Add Todo</button>
+    </div>
 );
 
 test('renders a text input', () => {
     const addTodo = shallow(<AddTodo/>);
     const input = addTodo.find('input');
     expect(input.type()).toEqual('input');
+});
+
+test('renders a button to add a new todo', () => {
+    const addTodo = shallow(<AddTodo/>);
+    const button = addTodo.find('button');
+    expect(button.type()).toEqual('button');
+    expect(button.render().text()).toEqual('Add Todo');
 });
