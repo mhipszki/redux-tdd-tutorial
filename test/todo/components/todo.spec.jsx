c2ad48dcb37cc2736c09f0f8493334a7eb27eb5a
Marton Hipszki <mhipszki@gmail.com>
2016-11-14 09:22:46 +0000

    Todo component handles click event

--- test/todo/components/todo.spec.jsx
+++ test/todo/components/todo.spec.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-const Todo = () => (
-    <li></li>
+const Todo = ({ onClick }) => (
+    <li onClick={onClick}></li>
 );
 
 test('renders a list element', () => {
     const todo = shallow(<Todo/>);
     expect(todo.type()).toBe('li');
+});
+
+test('handles click event', () => {
+    const onClick = jest.fn();
+    const todo = shallow(<Todo onClick={onClick}/>);
+    todo.simulate('click');
+    expect(onClick).toBeCalled();
 });
