cc79387bb35806260009370ee154204b65db72dd
Marton Hipszki <mhipszki@gmail.com>
2016-11-14 11:13:43 +0000

    TodoList component renders a list of Todos

--- test/todo/components/todo-list.spec.jsx
+++ test/todo/components/todo-list.spec.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-const TodoList = () => (
-    <ul></ul>
+const Todo = () => (<li></li>);
+
+const TodoList = ({ todos }) => (
+    <ul>
+        {todos.map(todo =>
+            <Todo key={todo.id}/>
+        )}
+    </ul>
 );
 
+const render = ({ todos = [] } = {}) => {
+    return shallow(
+        <TodoList todos={todos}></TodoList>
+    );
+};
+
 test('renders an unordered list', () => {
-    const list = shallow(<TodoList/>);
+    const list = render();
     expect(list.type()).toEqual('ul');
+});
+
+test('renders a list of todos', () => {
+    const todos = [{
+        id: 0,
+        text: 'todo 1'
+    },{
+        id: 1,
+        text: 'todo 2'
+    }];
+    const list = render({ todos });
+    const todoItems = list.find('Todo');
+    expect(todoItems.length).toBe(2);
 });
