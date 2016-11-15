a0bd22e24ef17c20aefd579a9e02d08cb7e8ad8c
Marton Hipszki <mhipszki@gmail.com>
2016-11-15 21:49:39 +0000

    FilterLink renders active Link when received and current filter are same

--- src/todo/components/filter-link.jsx
+++ src/todo/components/filter-link.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Link from './link';
 
-const FilterLink = () => (
-    <Link/>
-);
+const FilterLink = ({ filter, store, children }) => {
+    const currentFilter = store.getState().visibilityFilter;
+    const active = filter === currentFilter;
+    return (
+        <Link active={active}>{children}</Link>
+    );
+};
 
 export default FilterLink;
--- test/todo/components/filter-link.spec.jsx
+++ test/todo/components/filter-link.spec.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { createStore } from 'redux';
+import reducer from '../../../src/reducer/app';
 import FilterLink from '../../../src/todo/components/filter-link';
 
+const render = () => {
+    const store = createStore(reducer);
+    const currentFilter = store.getState().visibilityFilter;
+    return shallow(
+        <FilterLink
+            filter={currentFilter}
+            store={store}
+        >link</FilterLink>
+    );
+}
+
 test('renders a Link component', () => {
-    const filterLink = shallow(<FilterLink/>);
+    const filterLink = render();
     expect(filterLink.type().name).toEqual('Link');
+});
+
+test('passes children to Link component', () => {
+    const link = render();
+    expect(link.prop('children')).toEqual('link');
+});
+
+test('renders active Link when received and current filter are same', () => {
+    const link = render();
+    expect(link.prop('active')).toBe(true);
 });
