de336c08335e84955ac363f76b1d9da4b7d47a92
Marton Hipszki <mhipszki@gmail.com>
2016-11-16 09:46:20 +0000

    VisibleTodoList retrieves visible todos and passes them to TodoList

--- src/todo/components/visible-todo-list.jsx
+++ src/todo/components/visible-todo-list.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import TodoList from './todo-list';
+import getVisibleTodos from '../get-visible-todos';
 
 class VisibleTodoList extends React.Component {
     render() {
+        const { store } = this.props;
+        const { todos, visibilityFilter } = store.getState();
+        const visibleTodos = getVisibleTodos(todos, visibilityFilter);
         return (
-            <TodoList/>
+            <TodoList todos={visibleTodos} />
         );
     }
 }
--- test/todo/components/visible-todo-list.spec.jsx
+++ test/todo/components/visible-todo-list.spec.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { createStore } from 'redux';
+import reducer from '../../../src/reducer/app';
 import VisibleTodoList from '../../../src/todo/components/visible-todo-list';
 
+const render = ({
+    store = createStore(reducer)
+} = {}) => {
+    return shallow(<VisibleTodoList store={store} />);
+}
+
 test('renders a TodoList component', () => {
-    const list = shallow(<VisibleTodoList/>);
-    expect(list.type().name).toEqual('TodoList');
+    const visibleTodoList = render();
+    expect(visibleTodoList.type().name).toEqual('TodoList');
+});
+
+test('retrieves visible todos and passes them to TodoList', () => {
+    const store = createStore(reducer);
+    store.dispatch({ type: 'ADD_TODO', text: 'todo 1', id: 0 });
+    store.dispatch({ type: 'ADD_TODO', text: 'todo 2', id: 1 });
+    store.dispatch({ type: 'TOGGLE_TODO', id: 1 });
+    store.dispatch({ type: 'SET_VISIBILITY_FILTER', filter: 'SHOW_COMPLETED' });
+    const visibleTodoList = render({ store });
+    const todoList = visibleTodoList.find('TodoList');
+    expect(todoList.prop('todos')).toEqual([{
+        id: 1,
+        text: 'todo 2',
+        completed: true
+    }]);
 });
