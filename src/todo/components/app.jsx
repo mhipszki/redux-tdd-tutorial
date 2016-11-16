3eba1691d88b10d9acc693105f63550fc0fc8236
Marton Hipszki <mhipszki@gmail.com>
2016-11-16 10:40:09 +0000

    AddTodo component adds a new todo when button is clicked

--- src/todo/components/app.jsx
+++ src/todo/components/app.jsx
@@ -3,19 +3,10 @@ import AddTodo from './add-todo';
 import VisibleTodoList from './visible-todo-list';
 import Footer from './footer';
 
-let nextTodoId = 0;
-
 const TodoApp = ({ store }) => {
-    const addTodo = (text) => {
-        store.dispatch({
-            type: 'ADD_TODO',
-            id: nextTodoId++,
-            text
-        });
-    };
     return (
         <div>
-            <AddTodo onAddClick={addTodo} />
+            <AddTodo store={store} />
             <VisibleTodoList store={store} />
             <Footer store={store} />
         </div>
--- test/todo/components/add-todo.spec.jsx
+++ test/todo/components/add-todo.spec.jsx
@@ -1,32 +1,44 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
+import { createStore } from 'redux';
+import reducer from '../../../src/reducer/app';
 import AddTodo from '../../../src/todo/components/add-todo';
 
+const render = ({
+    store = createStore(reducer)
+} = {}) => mount(<AddTodo store={store} />);
+
 test('renders a text input', () => {
-    const addTodo = shallow(<AddTodo/>);
+    const addTodo = render();
     const input = addTodo.find('input');
     expect(input.type()).toEqual('input');
 });
 
 test('renders a button to add a new todo', () => {
-    const addTodo = shallow(<AddTodo/>);
+    const addTodo = render();
     const button = addTodo.find('button');
     expect(button.type()).toEqual('button');
     expect(button.render().text()).toEqual('Add Todo');
 });
 
-test('calls callback with input value when button is clicked', () => {
-    const onAddClick = jest.fn();
-    const addTodo = mount(<AddTodo onAddClick={onAddClick} />);
+test('adds a new todo when button is clicked', () => {
+    const store = createStore(reducer);
+    const addTodo = render({ store });
+    store.dispatch = jest.fn();
     const input = addTodo.find('input');
     const button = addTodo.find('button');
     input.node.value = 'new todo';
     button.simulate('click');
-    expect(onAddClick).toBeCalledWith('new todo');
+    expect(store.dispatch).toBeCalledWith({
+        type: 'ADD_TODO',
+        id: 0,
+        text: 'new todo'
+    });
 });
 
 test('clears input when button is clicked', () => {
-    const addTodo = mount(<AddTodo onAddClick={() => {}} />);
+    const store = createStore(reducer);
+    const addTodo = render({ store });
     const input = addTodo.find('input');
     const button = addTodo.find('button');
     input.node.value = 'new todo';
