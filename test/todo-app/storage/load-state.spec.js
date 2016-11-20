d4d3e1319513239c6d79888c30878b6a92bf618e
Marton Hipszki <mhipszki@gmail.com>
2016-11-20 18:21:27 +0000

    loadState returns undefined when state has not been saved to storage

--- test/todo-app/storage/load-state.spec.js
+++ test/todo-app/storage/load-state.spec.js
@@ -1,6 +1,10 @@
 const loadState = (storage) => {
     try {
-        return storage.getItem('state');
+        const state = storage.getItem('state');
+        if (state === null) {
+            return undefined;
+        }
+        return state;
     } catch (e) {
         return undefined;
     }
@@ -9,6 +13,14 @@ const loadState = (storage) => {
 test('returns undefined when storage is not accessible', () => {
     const fakeLocalStorage = {
         getItem: 'inaccessible'
+    };
+    const state = loadState(fakeLocalStorage);
+    expect(state).toBe(undefined);
+});
+
+test('returns undefined when state has not been saved to storage', () => {
+    const fakeLocalStorage = {
+        getItem: () => null
     };
     const state = loadState(fakeLocalStorage);
     expect(state).toBe(undefined);
