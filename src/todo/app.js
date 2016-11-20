4b9062ecca68a494943450a78747443994a9745d
Marton Hipszki <mhipszki@gmail.com>
2016-11-20 09:01:54 +0000

    using react-redux Provider instead of our own Provider component

--- src/todo/app.js
+++ src/todo/app.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import appReducer from '../reducer/app';
-import Provider from './components/provider';
 import TodoApp from './components/app';
 
 const store = createStore(appReducer);
