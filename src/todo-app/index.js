9cf96334fcfa76cb02f3ae34f8eca4128fb83c21
Marton Hipszki <mhipszki@gmail.com>
2016-11-21 08:29:15 +0000

    TodoApp uses configureStore module to create its store

--- src/todo-app/index.js
+++ src/todo-app/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import appReducer from './reducers/app';
 import TodoApp from './components/app';
-import { loadState, saveState } from './storage';
+import configureStore from './storage/configure-store';
 
-const persistedState = loadState(localStorage);
-
-const store = createStore(appReducer, persistedState);
-
-store.subscribe(() => {
-    const state = {
-        todos: store.getState().todos
-    };
-    saveState(state, localStorage);
-});
+const store = configureStore(window.localStorage);
 
 const app = (
     <Provider store={store}>
