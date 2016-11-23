1188fb7a701cefcf2027d1e6cb8189e0280944cb
Marton Hipszki <mhipszki@gmail.com>
2016-11-23 21:56:54 +0000

    Filterlink links to route with given filter

--- test/todo-app/components/filter-link/index.spec.jsx
+++ test/todo-app/components/filter-link/index.spec.jsx
@@ -3,8 +3,8 @@ import { shallow } from 'enzyme';
 import { Link } from 'react-router';
 import FilterLink from '../../../../src/todo-app/components/filter-link';
 
-const render = () => shallow(
-    <FilterLink>link</FilterLink>
+const render = ({ filter = 'filter' } = {}) => shallow(
+    <FilterLink filter={filter}>link</FilterLink>
 );
 
 test('renders a react router Link component', () => {
@@ -16,4 +16,18 @@ test('passes children to Link component', () => {
     const filterLink = render();
     const link = filterLink.find('Link');
     expect(link.prop('children')).toEqual('link');
+});
+
+test('links to base route when `all` filter is given', () => {
+    const filter = 'all';
+    const filterLink = render({ filter });
+    const link = filterLink.find('Link');
+    expect(link.prop('to')).toEqual('');
+});
+
+test('links to route with given filter otherwise', () => {
+    const filter = 'given filter';
+    const filterLink = render({ filter });
+    const link = filterLink.find('Link');
+    expect(link.prop('to')).toEqual('given filter');
 });
