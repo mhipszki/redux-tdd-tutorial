787d624cfd524272f7b6cde8e54c9ffdfb913430
Marton Hipszki <mhipszki@gmail.com>
2016-11-23 21:52:01 +0000

    FilterLink passes children to Link component

--- test/todo-app/components/filter-link/index.spec.jsx
+++ test/todo-app/components/filter-link/index.spec.jsx
@@ -4,10 +4,16 @@ import { Link } from 'react-router';
 import FilterLink from '../../../../src/todo-app/components/filter-link';
 
 const render = () => shallow(
-    <FilterLink/>
+    <FilterLink>link</FilterLink>
 );
 
 test('renders a react router Link component', () => {
     const filterLink = render();
-    expect(filterLink.contains(<Link/>)).toBe(true);
+    expect(filterLink.find(Link).length).toEqual(1);
+});
+
+test('passes children to Link component', () => {
+    const filterLink = render();
+    const link = filterLink.find('Link');
+    expect(link.prop('children')).toEqual('link');
 });
