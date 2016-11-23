dff7b766eddb6cd800139af1cb36d98acdd5693f
Marton Hipszki <mhipszki@gmail.com>
2016-11-23 22:00:24 +0000

    removed obsolete FilterLink / Redux mappings

--- src/todo-app/components/filter-link/map-dispatch-to-props.js
+++
@@ -1,9 +0,0 @@
-import { setVisibilityFilter } from '../../action-creators';
-
-const mapDispatchToProps = (dispatch, ownProps) => ({
-    onClick() {
-        dispatch(setVisibilityFilter(ownProps.filter));
-    }
-});
-
-export default mapDispatchToProps;
--- src/todo-app/components/filter-link/map-state-to-props.js
+++
@@ -1,5 +0,0 @@
-const mapStateToProps = (state, ownProps) => ({
-    active: ownProps.filter === state.visibilityFilter
-});
-
-export default mapStateToProps;
