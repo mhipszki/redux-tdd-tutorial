e1b4967122864a2b9dbf2f61511533ae9ca1ba08
Marton Hipszki <mhipszki@gmail.com>
2016-11-23 22:03:18 +0000

    removed obsolete setVisibilityFilter action creator

--- src/todo-app/action-creators/index.js
+++ src/todo-app/action-creators/index.js
@@ -1,3 +1,2 @@
 export { default as addTodo } from './add-todo';
-export { default as setVisibilityFilter } from './set-visibility-filter';
 export { default as toggleTodo } from './toggle-todo';
--- test/todo-app/action-creators/set-visibility-filter.spec.js
+++
@@ -1,9 +0,0 @@
-import { setVisibilityFilter } from '../../../src/todo-app/action-creators';
-
-test('returns a SET_VISIBILITY_FILTER action with given filter', () => {
-    const action = setVisibilityFilter('filter');
-    expect(action).toEqual({
-        type: 'SET_VISIBILITY_FILTER',
-        filter: 'filter'
-    });
-});
